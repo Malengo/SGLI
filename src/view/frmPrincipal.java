938c3239498d5b82bd617eb8b1e88c08e6c18380
contato@gunnarcorrea.com <contato@gunnarcorrea.com>
2014-03-18 14:39:07 -0300

    Atualização diária

--- src/view/frmPrincipal.java
+++ src/view/frmPrincipal.java
@@ -5,8 +5,7 @@
 package view;
 
 import DAO.conexao;
-import view.FrmClientes;
-
+import view.FrmCadastroPessoaFisica;
 /**
  *
  * @author Gunner Correa
@@ -89,10 +88,10 @@ public class frmPrincipal extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        FrmClientes form = new FrmClientes();
+         FrmCadastroPessoaFisica form = new FrmCadastroPessoaFisica();
         form.setLocationRelativeTo(null);
         form.setVisible(true);
-        //TEste
+        this.hide();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
